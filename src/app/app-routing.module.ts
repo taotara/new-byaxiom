dc7d57b1ee5feeec93eeec708dac7326541a274c
taotara <taotara@yahoo.com>
2023-07-31 12:36:13 +0100

    Prepared for deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -106,7 +106,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: "enabled"})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      useHash: true,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
